e296e8fac2488b876a29580185dcfa5fdc5e05b0
Kovucin <ai.arenas@duocuc.cl>
2023-08-25 14:35:17 -0400

    Login and Animations

--- proyecto/src/app/home/home.page.ts
+++ proyecto/src/app/home/home.page.ts
@@ -1,27 +1,46 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,ViewChild} from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
+import { IonAvatar,AnimationController} from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  //ViewChild(TipoComponente,{read:ElementRef}) nombre!:ElementRef<HTMLTipoObjetoElement>
+  @ViewChild(IonAvatar,{read:ElementRef}) avatar!:ElementRef<HTMLIonAvatarElement>;
+ 
+  constructor(private router: Router,private animationCtrl:AnimationController) { }
+  private animation!:Animation;
+  public mensaje = ""
 
-  constructor(private router: Router) { }
-
-  public mensaje = "Hola soy el nuevo mensaje"
-
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl.create()
+    .addElement(this.avatar.nativeElement)
+    .duration(1500) 
+    .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+    .fromTo('opacity', '1', '0.2');
+  }
   user = {
     usuario: "",
     password: ""
   }
-
+  
+  avatarPlay(){
+    this.animation.play();
+  }
 
   enviarInformacion() {
-    let navigationExtras: NavigationExtras = {
-      state: { user: this.user }
+    if (this.user.usuario != "") {
+      let navigationExtras: NavigationExtras = {
+        state: { user: this.user }
+      }
+      this.router.navigate(['/login'], navigationExtras);
+    } else {
+      this.mensaje = "Debe ingresar sus credenciales";
     }
-    this.router.navigate(['/login'], navigationExtras);
   }
   mostrarConsola() {
     console.log(this.user);
