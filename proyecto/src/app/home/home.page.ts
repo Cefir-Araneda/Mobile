6a8a4b756081655f9ca8b3b62b44d10ae4917520
Cefir-Araneda <fr.aranedag@duocuc.cl>
2023-09-15 15:44:55 -0300

    Card animada

--- proyecto/src/app/home/home.page.ts
+++ proyecto/src/app/home/home.page.ts
@@ -1,8 +1,9 @@
-import { Component,ViewChild,ElementRef} from '@angular/core';
+import { Component,ViewChild,ElementRef,ViewChildren} from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
-import { IonAvatar, IonTitle } from '@ionic/angular';
+import { IonAvatar } from '@ionic/angular';
 import type { Animation } from '@ionic/angular';
-import { AnimationController } from '@ionic/angular';
+import type { QueryList } from '@angular/core';
+import { AnimationController, IonCard } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,32 +11,37 @@ import { AnimationController } from '@ionic/angular';
 })
 export class HomePage {
   @ViewChild(IonAvatar,{read:ElementRef}) avatar!:ElementRef<HTMLIonAvatarElement>;
+  @ViewChild(IonCard,{read:ElementRef}) card!: ElementRef<HTMLIonCardElement>;
 
   private animation!:Animation;
   constructor(private router: Router,private animationCtrl:AnimationController) { }
   public mensaje = ""
 
   ngAfterViewInit() {
-    this.animation = this.animationCtrl.create()
-    .addElement(this.avatar.nativeElement)
-    .duration(5000)
-    .iterations(Infinity)
-    .keyframes([
-      {offset:0, transform:'translateX(0px)',opacity:'1'},
-      {offset:0.25, transform:'translateX(100px)',opacity:'0.2'},
-      {offset:0.50, transform:'translateX(0px)',opacity:'1'},
-      {offset:0.75, transform:'translateX(-100px)',opacity:'0.2'},
-      {offset:1, transform:'translateX(0px)',opacity:'1'},
-    ])
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1500)
+      .iterations(1)
+      .direction('alternate')
+      .fromTo('background', 'white', 'var(--background)');
+  }
+
+  playAnimation() {
+    this.animation.play();
+  }
+
+  pause() {
+    this.animation.pause();
+  }
+
+  stop() {
+    this.animation.stop();
   }
 
   user = {
     usuario: "",
     password: ""
-  }
-  
-  playAnimation(){
-    this.animation.play();
   }
 
   enviarInformacion() {
