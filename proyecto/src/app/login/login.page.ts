c9e7d73473811d6edc87db677db81190c9edd944
Aidan Asher <ai.arenas@duocuc.cl>
2023-11-14 12:20:08 -0300

    a
    a

--- proyecto/src/app/login/login.page.ts
+++ proyecto/src/app/login/login.page.ts
@@ -86,63 +86,69 @@ export class LoginPage {
       });
   }
 
-forgot() {
-  this.router.navigate(['forgot']);
-}
-
-cancel() {
-  this.modal.dismiss(null, 'cancel');
-}
-
-confirm() {
-  if (this.credentials.username === '' || this.credentials.password === '') {
-    console.log("Algun campo no tiene valor");
-    this.mensaje = "Algun campo no tiene valor";
-    setTimeout(() => {
-      this.mensaje = "";
-    }, 2500);
-  } else if (this.credentials.password.length < 8) {
-    console.log("Contraseña con menos de 8 caracteres");
-    this.mensaje = "Contraseña con menos de 8 caracteres";
-    setTimeout(() => {
-      this.mensaje = "";
-    }, 2500);
+  forgot() {
+    this.router.navigate(['forgot']);
   }
-  else {
-    // Verificar si el nombre de usuario ya existe
-    this.api.getPostsL().pipe(first()).subscribe(
-      (users) => {
-        const existeUsuario = users.find((user: any) => user.username === this.credentials.username);
-        if (existeUsuario) {
-          console.log("Nombre de usuario ya existe");
-          this.mensaje = "Usuario existente";
-          setTimeout(() => {
-            this.mensaje = "";
-          }, 2500);
-        } else {
-          // El nombre de usuario no existe, proceder con el registro
-          console.log(this.credentials);
-          this.api.createPostL(this.credentials).subscribe(
-            (success) => {
-              this.mensaje = "Registro Exitoso";
-              console.log("Funcionaaaa :D");
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    if (this.credentials.username === '' || this.credentials.password === '') {
+      console.log("Algun campo no tiene valor");
+      this.mensaje = "Algun campo no tiene valor";
+      setTimeout(() => {
+        this.mensaje = "";
+      }, 2500);
+    } else if (this.credentials.password.length < 8) {
+      console.log("Contraseña con menos de 8 caracteres");
+      this.mensaje = "Contraseña con menos de 8 caracteres";
+      setTimeout(() => {
+        this.mensaje = "";
+      }, 2500);
+    }else if (this.credentials.rol === '') {
+        console.log("Ningun rol seleccionado");
+        this.mensaje = "Ningun rol seleccionado";
+        setTimeout(() => {
+          this.mensaje = "";
+        }, 2500);
+      }
+      else {
+        // Verificar si el nombre de usuario ya existe
+        this.api.getPostsL().pipe(first()).subscribe(
+          (users) => {
+            const existeUsuario = users.find((user: any) => user.username === this.credentials.username);
+            if (existeUsuario) {
+              console.log("Nombre de usuario ya existe");
+              this.mensaje = "Usuario existente";
               setTimeout(() => {
                 this.mensaje = "";
+              }, 2500);
+            } else {
+              // El nombre de usuario no existe, proceder con el registro
+              console.log(this.credentials);
+              this.api.createPostL(this.credentials).subscribe(
+                (success) => {
+                  this.mensaje = "Registro Exitoso";
+                  console.log("Funcionaaaa :D");
+                  setTimeout(() => {
+                    this.mensaje = "";
+                  }, 2000);
+                },
+                (err) => {
+                  console.error(err);
+                }
+              );
+              setTimeout(() => {
+                this.modal.dismiss(this.credentials.username, 'confirm');
               }, 2000);
-            },
-            (err) => {
-              console.error(err);
             }
-          );
-          setTimeout(() => {
-            this.modal.dismiss(this.credentials.username, 'confirm');
-          }, 3000);
-        }
-      },
-      (error) => {
-        console.error(error);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
       }
-    );
+    }
   }
-}
-}
